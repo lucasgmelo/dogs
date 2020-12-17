4a9d9c2bb5238aabc19a603ffebfe5ec797bac62
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-17 02:07:12 -0300

    feat(header): style header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { 
-    Title
+    Head,
+    Nav,
+    Logo,
+    Login,
 } from './Header_styles'
 import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 
 const Header = () => {
     return (
-        <div>
-            <nav className="container" >
-                <Link to="/">Home</Link>
-                <Link to="/login">Login / Criar</Link>
+        <Head>
+            <Nav className="container" >
+                <Link to="/" aria-label="Dogs - Home" >
+                    <Logo>
+                        <Dogs />
+                    </Logo>
+                </Link>
+                <Link to="/login">
+                    <Login>
+                        Login / Criar
+                    </Login>
+                    </Link>
 
-            </nav>
-        </div>
+            </Nav>
+        </Head>
     )
 }
 
--- src/Components/Header_styles.js
+++ src/Components/Header_styles.js
@@ -1,9 +1,41 @@
 import styled from 'styled-components';
 
-const Title = styled.h1`
-color: #912222;
+const Head = styled.header`
+    box-shadow: 0px 1px 1px 0 rgba(0, 0, 0, .1);
+    padding: .5rem 0;
+    position: fixed;
+    width: 100%;
+    z-index: 100;
+    background: white;
+    top: 0;
+`;
+
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Logo = styled.div`
+    padding: 0.5rem 0;
+`;
+
+const Login = styled.a`
+    & {
+        color: #333;
+    }
+    &:after {
+        content: "";
+        display: inline-block;
+        width: 14px;
+        height:17px;
+        background: url('../Assets/usuario.svg')
+    }
 `;
 
 export {
-    Title,
+    Head,
+    Nav,
+    Logo,
+    Login,
 }
