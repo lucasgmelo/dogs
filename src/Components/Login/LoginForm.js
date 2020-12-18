dc8a0a1214c0a57687cb0411737486882c43e8cd
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-18 02:14:38 -0300

    feat(login): connect API

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const LoginForm = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit (event) {
+        event.preventDefault();
+        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+            method: 'POST',
+            headers: {
+                ContentType: 'application-json'
+            },
+            body: JSON.stringify({username, password}),
+        }).then(response => {
+            console.log(response);
+            return response.json();
+        }).then(json => {
+            console.log(json);
+        })
+    }
+
     return (
-        <div>
+        <section>
+            <h1>Login</h1>
+            <form action="" onSubmit={handleSubmit}> 
+                <input type="text" 
+                value={username}
+                onChange={({target})=>{
+                    setUsername(target.value);
+                }} />
+                <input type="password" 
+                value={password}
+                onChange={({target})=>{
+                    setPassword(target.value);
+                }} />
+                <button>Enviar</button>
+            </form>
             <Link to="/login/create" >Cadastro</Link>
-        </div>
+        </section>
     )
 }
 
