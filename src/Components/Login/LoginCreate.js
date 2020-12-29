06460f9ed35ef74349e3145bf35eb9c9450dda60
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2020-12-29 16:28:10 -0300

    feat(register): add input and btn

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Title } from '../MainStyles'
+import Input from '../Forms/Input/Input'
+import Button from '../Forms/Button/Button'
+
 
 const LoginCreate = () => {
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
         <section className="animeLeft">
             <Title>Cadastre-se</Title>
-            <form onSubmit={handleSubmit}></form>
+            <form onSubmit={handleSubmit}>
+                <Input label="Usuário" type="text" name="username"/>
+                <Button childrens="Cadastrar"/>
+            </form>
         </section>
     )
 }
