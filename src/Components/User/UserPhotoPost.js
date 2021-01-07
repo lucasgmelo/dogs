f5794ed9395c34e526d183725e7d5ee480882190
Lucas Gabriel Rodrigues de Melo <contatolucasgmelo@gmail.com>
2021-01-07 18:14:42 -0300

    feat(photo-post): add preview

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -30,6 +30,7 @@ const UserPhotoPost = () => {
 
     function handleImgChange({target}){
         setImg({
+            preview: URL.createObjectURL(target.files[0]),
             raw: target.files[0],
 
         })
@@ -42,10 +43,15 @@ const UserPhotoPost = () => {
                 <Input label="Nome" type="text" name="nome" {...nome}/>
                 <Input label="Peso" type="number" name="peso" {...peso}/>
                 <Input label="Idade" type="number" name="idade" {...idade}/>
-                <input type="file" name="img" id="img" onChange={handleImgChange}/>
+                <input className={styles.file} type="file" name="img" id="img" onChange={handleImgChange}/>
 
                 <Button childrens="enviar"/>
             </form>
+            <div>
+                {img.preview && <div className={styles.preview} style={{backgroundImage: 'url(`${img.preview}`)'}}>
+                    
+                    </div>}
+            </div>
 
         </section>
     )
